1f484cedccb652d19b9193c82a3ed4e80a930242
Sleppyiecat <mruczek70@gmail.com>
2021-10-30 22:19:00 +0200

    davinci: create vendorsetup.sh * this will be usefull if someone (for example me) is too lazy to clone all trees one by one

---
+++ vendorsetup.sh
@@ -0,0 +1,25 @@
+KERNEL=https://github.com/ArianK16a/android_kernel_xiaomi_sm6150
+HW=https://github.com/LineageOS/android_hardware_xiaomi
+VENDOR=https://github.com/xiaomi-sm6150/android_vendor_xiaomi
+COMMON=https://github.com/Nezuro-Network/android_device_xiaomi_sm6150-common
+
+if [ ! -d "kernel/xiaomi/sm6150" ]
+then
+	git clone $KERNEL kernel/xiaomi/sm6150 --depth=1
+fi
+
+if [ ! -d "hardware/xiaomi" ]
+then
+	git clone $HW hardware/xiaomi -b lineage-19.0 --depth=1
+fi
+
+if [ ! -d "device/xiaomi/sm6150-common" ]
+then
+        git clone $COMMON device/xiaomi/sm6150-common --depth=1
+fi
+
+if [ ! -d "vendor/xiaomi" ]
+then
+	git clone $VENDOR vendor/xiaomi --depth=1
+fi
+
